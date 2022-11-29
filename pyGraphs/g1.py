383d9067c371cdcb809d9aceab5b90d140196234
Dhiraj Upadhyaya <dupadhyaya@users.noreply.github.com>
2022-11-29 16:02:58 +0530

    a

---
+++ pyGraphs/g1.py
@@ -0,0 +1,29 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pydataset import data
+
+
+#bar
+# x-coordinates of left sides of bars 
+left = [1, 2, 3, 4, 5]
+  
+# heights of bars
+height = [10, 24, 36, 40, 5]
+  
+# labels for bars
+tick_label = ['one', 'two', 'three', 'four', 'five']
+  
+# plotting a bar chart
+plt.bar(left, height, tick_label = tick_label,width = 0.8, color = ['red', 'green'])
+  
+# naming the x-axis
+plt.xlabel('x - axis')
+# naming the y-axis
+plt.ylabel('y - axis')
+# plot title
+plt.title('My bar chart!')
+  
+# function to show the plot
+plt.show()
